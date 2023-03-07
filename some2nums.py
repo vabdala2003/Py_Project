868e80b9d818fd61e398beaf8f3117ea13b6aa61
vabdala2003 <113123693+vabdala2003@users.noreply.github.com>
2023-03-07 18:49:23 -0300

    Add files via upload

---
+++ some2nums.py
@@ -0,0 +1,7 @@
+print(input("Aperte enter para continuar"))
+
+valor1 = int(input("Qual o primeiro número? "))
+valor2 = int(input("Qual o segundo número? "))
+
+print("calculando...")
+print(valor1 + valor2)
